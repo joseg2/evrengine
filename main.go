b16e97ec6b249c006df277b4bc6da67012924ce3
j@example.com <you@example.com>
2025-03-16 10:40:01 +1100

    added environment variable and location support

--- main.go
+++ main.go
@@ -3,27 +3,56 @@ package main
 import (
 	"fmt"
 	"log"
-  "os"
+	"os"
 	"os/exec"
 )
 
-func main(){
-  yamels,err := readYamlEntries(os.Args[1])
-   if err != nil {
-    log.Fatal(err)
+func main() {
+
+	// Initial variables
+	var yaml_file string
+	location := os.Getenv("LOCATION")
+
+  if location == "" {
+    log.Fatal("ERROR: missing LOCATION environment variable")
   }
 
-  for _, entry := range yamels.Section {
-    item := entry.Item
-    command := entry.Command
-    fmt.Printf("ITEM: %+v\n  Command:\n%+v\n",item,command)
-    cmd := exec.Command("bash", "-c", command)
-    stdout, err := cmd.Output()
-    if err != nil {
-      log.Fatal(err)
-      return
-    }
-    fmt.Println(string(stdout))
-  }
+	// Determine argument precedence, cli arguments override environment variables
+	if len(os.Args) != 2 {
+		if os.Getenv("YAML") == "" {
+			log.Fatal("ERROR: missing yaml file argument or YAML environment variable")
+		} else {
+			yaml_file = os.Getenv("YAML")
+		}
+	} else if os.Args[1] != "" {
+		yaml_file = os.Args[1]
+	}
+
+	// Read yaml file into memory
+	yamels, err := readYamlEntries(yaml_file)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Display settings before starting
+	if os.Getenv("VERBOSE") == "true" {
+		log.Printf("LOCATION='%s'\n", location)
+		log.Printf("yaml_file='%s'\n", yaml_file)
+	}
+
+	// Execute commands
+	for _, entry := range yamels.Section {
+		if location == entry.Location {
+			item := entry.Item
+			command := entry.Command
+			fmt.Printf("ITEM: %+v\n  Command:\n%+v\n", item, command)
+			cmd := exec.Command("bash", "-c", command)
+			stdout, err := cmd.Output()
+			if err != nil {
+				log.Fatal(err)
+				return
+			}
+			fmt.Println(string(stdout))
+		}
+	}
 }
-
