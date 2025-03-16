a3c7ea8ff5f667280cebe53d901d762a48903064
j@example.com <you@example.com>
2025-03-16 16:41:57 +1100

    Added ansi colour and camera emoji

--- main.go
+++ main.go
@@ -7,15 +7,24 @@ import (
 	"os/exec"
 )
 
+const colourR = "\033[0;31m"
+const colourG = "\033[0;32m"
+const colourB = "\033[0;34m"
+const colourC = "\033[0;36m"
+const colourM = "\033[0;35m"
+const colourY = "\033[0;33m"
+const colourK = "\033[0;00m"
+const emoji_camera = '\U0001F4F7'
+
 func main() {
 
 	// Initial variables
 	var yaml_file string
 	location := os.Getenv("LOCATION")
 
-  if location == "" {
-    log.Fatal("ERROR: missing LOCATION environment variable")
-  }
+	if location == "" {
+		log.Fatal("ERROR: missing LOCATION environment variable")
+	}
 
 	// Determine argument precedence, cli arguments override environment variables
 	if len(os.Args) != 2 {
@@ -44,15 +53,17 @@ func main() {
 	for _, entry := range yamels.Section {
 		if location == entry.Location {
 			item := entry.Item
+			description := entry.Description
 			command := entry.Command
-			fmt.Printf("ITEM: %+v\n  Command:\n%+v\n", item, command)
+			fmt.Printf("%v%+v %v%+v\n  %vCommand:\n%v%+v%v\n", colourY, item, colourR, description, colourM, colourC, command, colourK)
 			cmd := exec.Command("bash", "-c", command)
 			stdout, err := cmd.Output()
 			if err != nil {
-				log.Fatal(err)
+				log.Fatal("Command: ", err)
 				return
 			}
 			fmt.Println(string(stdout))
+			fmt.Printf("%c\n", emoji_camera)
 		}
 	}
 }
